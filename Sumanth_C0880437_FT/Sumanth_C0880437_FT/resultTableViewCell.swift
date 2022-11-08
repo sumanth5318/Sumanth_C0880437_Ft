5d68bf0ddd7d617047c2682064ecf90f1730aa08
sumanth <saisumanth5318@gmail.com>
2022-11-07 21:13:00 -0500

    image added

---
+++ Sumanth_C0880437_FT/Sumanth_C0880437_FT/resultTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  resultTableViewCell.swift
+//  Sumanth_C0880437_FT
+//
+//  Created by Sai Sumanth Nissankara rao on 2022-11-07.
+//
+
+import UIKit
+
+class resultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
